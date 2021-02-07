7591ddf1d652995fd0eb80fc80e6bef03d1628a4
Konstantin <05kostman05@gmail.com>
2021-02-07 20:02:47 +0300

    1
    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button @click="click">sahjdk</button>
+    <button @click="press">sahjdk</button>
   </div>
 
 </template>
@@ -14,9 +14,19 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
  methods:{
-   click:function(){
+   press:function(){
 this.axios.get("http://37.77.104.246/api/jsonstorage/?id=2716ef404c324ddc5c9fea59215e724a").then(response =>{
-    console.log(response.data);
+ let users = response.data;
+ let found = false;
+ for (let index in users){
+   if (this.login == users[index].login&& this.password == users[index].password){
+     this.$router.push('/users/' + this.myId);
+     found =  true;
+     break;
+
+   }
+ }
+ if (!found) window.alert('Неверный логин или пароль');
 })
    }
  }
--- src/views/login.vue
+++ src/views/login.vue
@@ -23,3 +23,24 @@
         </v-card>
     </div>
 </template>
+<script>
+export default {
+ methods:{
+  authenticate(){
+this.axios.get("http://37.77.104.246/api/jsonstorage/?id=2716ef404c324ddc5c9fea59215e724a").then(response =>{
+ let users = response.data;
+ let found = false;
+ for (let index in users){
+   if (this.login == users[index].login&& this.password == users[index].password){
+     this.$router.push('/users/' + this.myId);
+     found =  true;
+     break;
+
+   }
+ }
+ if (!found) window.alert('Неверный логин или пароль');
+})
+   }
+ }
+}
+</script>
