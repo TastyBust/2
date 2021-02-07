a77f4f5f546cd858cf5e00ad8b8d297d97eb180f
Konstantin <05kostman05@gmail.com>
2021-02-07 19:38:34 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="click">sahjdk</button>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+ methods:{
+   click:function(){
+this.axios.get("http://37.77.104.246/api/jsonstorage/?id=7c1ddaec79baa8a4f419449d062a24a7").then(response =>{
+    console.log(response.data);
+})
+   }
+ }
+}
+</script>
